2c338580d82b3b0ec7be7d9c94b381b3da581648
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2026-03-09 18:28:32 +0300

    phone number validation added

--- app/src/main/java/com/example/fisrtproject/MainActivity.kt
+++ app/src/main/java/com/example/fisrtproject/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.fisrtproject.ui.theme.FisrtProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +28,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FisrtProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    FirstScreen()
                 }
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun isValidPhoneNumber(phone: String): Boolean {
+    val cleanPhone = phone.replace(Regex("[\\s\\-()]+"), "")
+    val phoneRegex = Regex("^\\+?[0-9]{7,15}$")
+    return phoneRegex.matches(cleanPhone)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FisrtProjectTheme {
-        Greeting("Android")
+fun FirstScreen() {
+    var text by remember { mutableStateOf("") }
+    var phoneError by remember { mutableStateOf<String?>(null) }
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+
     }
 }
