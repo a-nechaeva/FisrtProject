caf7a207a3ee8a7f3e1bf3830bb0677d21d85653
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2026-03-10 19:30:27 +0300

    app list screen rough draft

--- app/src/main/java/com/example/fisrtproject/ui/screens/AppListScreen.kt
+++ app/src/main/java/com/example/fisrtproject/ui/screens/AppListScreen.kt
@@ -1,2 +1,55 @@
 package com.example.fisrtproject.ui.screens
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.fisrtproject.data.AppData
+import com.example.fisrtproject.ui.components.AppListItem
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppListScreen(
+    onAppClick: (String) -> Unit
+) {
+    val apps = AppData.appList
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("RuStore")},
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        }
+    ) {
+        paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentPadding = PaddingValues(vertical = 8.dp)
+        ) {
+            items(
+                count = apps.size,
+                key = { index -> apps[index].id }
+            ) {
+                index ->
+                AppListItem(
+                    app = apps[index],
+                    onClick = { onAppClick(apps[index].id)}
+                )
+            }
+        }
+    }
+}
