42961b5e671632e13c25346f8cfe38ef34758168
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2026-03-11 11:40:35 +0300

    navigation rough draft

--- app/src/main/java/com/example/fisrtproject/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/fisrtproject/navigation/AppNavigation.kt
@@ -1,2 +1,58 @@
 package com.example.fisrtproject.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.fisrtproject.data.AppData
+import com.example.fisrtproject.ui.screens.AppDetailScreen
+import com.example.fisrtproject.ui.screens.AppListScreen
+
+sealed class Screen(val route: String) {
+    object AppList : Screen("app_list")
+    object AppDetail : Screen("app_detail/{appId}") {
+        fun createRoute(appId: String) = "app_detail/$appId"
+    }
+}
+@Composable
+fun AppNavigation(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.AppList.route
+    ) {
+        composable(Screen.AppList.route) {
+            AppListScreen(
+                onAppClick = { appId ->
+                    navController.navigate(Screen.AppDetail.createRoute(appId))
+                }
+            )
+        }
+        composable(
+            route = Screen.AppDetail.route,
+            arguments = listOf(
+                navArgument("appId") {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            backStackEntry ->
+            val appId = backStackEntry.arguments?.getString("appId")?:return@composable
+            val app = AppData.getAppById(appId)
+
+            if (app != null) {
+                AppDetailScreen(
+                    app = app,
+                    onBackClick = { navController.popBackStack() }
+                )
+            } else {
+                AppListScreen(
+                    onAppClick = { navController.popBackStack() }
+                )
+            }
+        }
+    }
+}
